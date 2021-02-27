01a172991aee0c752633d213fecf106b68a3a73d
Pazia Ramirez <pazia008@gmail.com>
2021-02-26 20:15:53 -0600

    moose is questioning more things

--- Program.cs
+++ Program.cs
@@ -11,15 +11,62 @@ MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
 // Ask a question
-bool isTrue = MooseAsks("Is Canada real?");
-if (isTrue)
+CanadaQuestion();
+EnthusiasticQuestion();
+LoveCSharpQuestion();
+SecretQuestion();
+
+void CanadaQuestion()
 {
-    MooseSays("Really? It seems very unlikely.");
-}
-else
+    bool isTrue = MooseAsks("Is Canada real?");
+    if (isTrue)
+    {
+        MooseSays("Really? It seems very unlikely.");
+    }
+    else
+    {
+        MooseSays("I  K N E W  I T !!!");
+    }
+};
+
+void EnthusiasticQuestion()
 {
-    MooseSays("I  K N E W  I T !!!");
-}
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+};
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+};
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+};
 
 
 void MooseSays(string message)
